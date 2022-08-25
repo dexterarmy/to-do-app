62cc78486718f1d50c2b7c23cb2a0efde398094f
dexterarmy <immohitsharma8@gmail.com>
2022-08-25 11:46:31 +0530

    served static files and enabled views engine

--- project-root/app.js
+++ project-root/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use("/static", express.static("public"));
+
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("todo.ejs");
+});
 
 module.exports = app;
