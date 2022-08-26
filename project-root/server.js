0fe0fac5e231ffdeb7a484bca9719408145e4c2b
dexterarmy <immohitsharma8@gmail.com>
2022-08-26 12:57:38 +0530

    connected the application with Atlas database

--- project-root/server.js
+++ project-root/server.js
@@ -1,3 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.PASSWORD);
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    // console.log(conObj.connections);
+    console.log("database connection successful");
+  })
+  .catch((err) => console.log(err.name, err.message));
 
 app.listen(3000, () => console.log("listening on port 3000"));
