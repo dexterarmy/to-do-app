bced687f596adcab054ba73883468f4f4759816e
dexterarmy <immohitsharma8@gmail.com>
2022-08-26 12:59:21 +0530

    created the POST middleware to create data in the database

--- project-root/app.js
+++ project-root/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Todo = require("./models/todoModel");
 
 const app = express();
 
@@ -6,8 +7,23 @@ app.use("/static", express.static("public"));
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+// GET
 app.get("/", (req, res) => {
   res.render("todo.ejs");
+});
+
+//POST
+app.post("/", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    await Todo.create(req.body);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err.name, err.message);
+    res.redirect("/");
+  }
 });
 
 module.exports = app;
